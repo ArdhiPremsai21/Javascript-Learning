4d465a5b40bac0b91a8072cd4ec1c8ecb8cdcbc4
Premsai_Ardhi <89005009+Unanynus@users.noreply.github.com>
2024-07-17 17:29:30 +0000

    "Datatype conversion"

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+console.log("this is the first code of conversion operation");
+
+// let score = "33abs";
+// let score = null;
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"333" => 333 Number
+//"333abc" => NaN (Not a Number)
+//true => 1 ; false => 0
+
+// let isLoggedIn = "string";
+let isLoggedIn = "";
+
+let isLoggedBoolean = Boolean(isLoggedIn);
+
+console.log(isLoggedBoolean);
+
+// 1 => true; 0 => false in boolean
+// "" => false; "Premsai" => true in boolean
+
+
+let someNumber = 77;
+
+let someString  = String(someNumber);
+
+console.log(someString);
+console.log(typeof someString);
+
+
+
