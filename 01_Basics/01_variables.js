2d1646981f1c918b843efb8d4f44f0147eb82690
Premsai_Ardhi <89005009+Unanynus@users.noreply.github.com>
2024-07-17 08:38:33 +0000

    "the first basics"

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 13232
+let accountEmail = "premsai@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai";
+
+let accountState;
+
+console.log("this is first code");
+// console.log("this is the account ID : ", accountId);
+// console.log("this is the account email : ", accountEmail);
+// console.log("this is the account password : ", accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState]);
+
+// accountId = 23;
+
+    /* 
+        The var which was previously used in javascript to declare variables it had one major problem of 
+        not realizing the scope of variables which caused the problem for the varibales with the same initials 
+        outside the scope slo to solve this issue javascript came up with let so there are officially to ways to 
+        declare a variable "let " & "const" for declaring constants
+    */
+
+console.log('this is cool right');
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //It will basically use the newer version of the javascript basically the ecmascript 
+// alert(3 + 4)  We are using node JS not any browser
+    
+
+console.log("this is the datatype of the javascript");
+console.log("this is smething else althogether");
+
+let name = "Premsai Ardhi";
+let age = 22;
+let isLiggedIn = true;
+
+// number => power 2 pow 2 to 54
+// bigint =>
+// string => "All is well"
+// boolean => true/false
+// null => standalone
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof {name: "Premsai"}); //object
+console.log(typeof age);   //number
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
